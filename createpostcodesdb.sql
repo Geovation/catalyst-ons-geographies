321fdbe3a29f18aeba1eb3731487164d23b3a07f
Dave <8709533+DaveBathnes@users.noreply.github.com>
2025-01-08 15:47:35 +0000

    Removing raw msoa and lsoa data from the db

--- createpostcodesdb.sql
+++ createpostcodesdb.sql
@@ -1,9 +1,6 @@
 INSTALL spatial;
 LOAD spatial;
 
-CREATE TABLE msoas AS SELECT MSOA21CD as msoa21cd, geometry FROM read_parquet('data/msoas.parquet');
-CREATE TABLE lsoas AS SELECT LSOA21CD as lsoa21cd, geometry FROM read_parquet('data/lsoas.parquet');
-
 CREATE TABLE postcodes AS SELECT pcd,doterm,oscty,ced,oslaua,osward,oseast1m,osnrth1m,osgrdind,ctry,rgn,pcon,oa11,lsoa11,msoa11,bua24,ru11ind,imd,oa21,lsoa21,msoa21,lat,long FROM read_parquet('data/ons-postcode-directory.parquet');
 
 -- On the postcodes table create a geometry column from longitude and latitude using st_point
